6805f41febb0b8c4a033bf34d913ba0eaad022f8
joseenriquez1404 <manuel.enriquezr@uanl.edu.mx>
2024-04-18 17:19:43 -0600

    Practica 9

--- miApp-2132982/src/app/app.module.ts
+++ miApp-2132982/src/app/app.module.ts
@@ -8,9 +8,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListaAlumnosComponent } from './lista-alumnos/lista-alumnos.component';
 
+import { ProductosComponent } from './productos/productos.component';
+import { HttpClientModule } from '@angular/common/http';
+
+
 @NgModule({
-  declarations: [AppComponent,ListaAlumnosComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  declarations: [AppComponent,ListaAlumnosComponent, ProductosComponent],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
